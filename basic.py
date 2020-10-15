90c91e4c6003db8b5295efc07e6371ea6510c88e
AHardik <72928008+AHardik@users.noreply.github.com>
2020-10-15 19:48:38 +0530

    Create basic.py
    Add one python file in which add some numpy array's concepts.

---
+++ basic.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# it is 1D array in  numpy
+create_arr = np.array([1,2,2])
+
+# create 2D array in numpy
+create_arr2 = np.array([[1,2,2], [3,4,5]])
+
+
+print(f"1D array is:{create_ar} \n 2D array is: {create_ar2}")
